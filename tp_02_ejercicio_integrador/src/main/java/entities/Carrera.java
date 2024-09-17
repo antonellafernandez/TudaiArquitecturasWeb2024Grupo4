39a97603f5a263ce14a69863747fed7ed1d6ee3b
Daniela Fernández <liquiddookie.17@gmail.com>
2024-09-17 13:45:56 -0300

    Updated.

---
+++ tp_02_ejercicio_integrador/src/main/java/entities/Carrera.java
@@ -0,0 +1,25 @@
+package entities;
+
+
+import java.util.Set;
+
+@AllArgsContructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "carreras")
+public class Carrera {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @ManyToMany(mappedBy = "carreras")
+    private Set<Estudiante> estudiantes;
+
+}
+
---
+++ tp_02_ejercicio_integrador/src/main/java/entities/Estudiante.java
@@ -0,0 +1,34 @@
+package entities;
+
+import java.util.List;
+import java.util.Set;
+
+@AllArgsContructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "estudiantes")
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombres;
+    private String apellido;
+    private int edad;
+    private String genero;
+    private String numeroDocumento;
+    private String ciudadResidencia;
+    private String numeroLibretaUniversitaria;
+
+    //esto crea la tabla intermedia entre carrera y estudiante
+    @ManyToMany
+    @JoinTable(
+            name = "estudiantes_carreras",
+            joinColumns = @JoinColumn(name = "estudiante_id"),
+            inverseJoinColumns = @JoinColumn(name = "carrera_id")
+    )
+    private Set<Carrera> carreras;
+    private boolean graduado;
+}
