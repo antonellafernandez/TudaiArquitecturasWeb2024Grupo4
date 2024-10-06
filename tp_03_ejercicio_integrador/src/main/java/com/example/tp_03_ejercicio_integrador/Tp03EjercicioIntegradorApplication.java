c5858da1746cab6bc86be549fe8a73387cb18d5f
Daniela Fernández <liquiddookie.17@gmail.com>
2024-10-06 16:16:14 -0300

    Updated.

---
+++ tp_03_ejercicio_integrador/src/main/java/com/example/tp_03_ejercicio_integrador/Tp03EjercicioIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_03_ejercicio_integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp03EjercicioIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp03EjercicioIntegradorApplication.class, args);
+	}
+
+}
---
+++ tp_03_ejercicio_integrador/src/test/java/com/example/tp_03_ejercicio_integrador/Tp03EjercicioIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_03_ejercicio_integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp03EjercicioIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
