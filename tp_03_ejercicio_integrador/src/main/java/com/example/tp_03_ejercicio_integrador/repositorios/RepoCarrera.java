ff0ec0d42ee76340ca7ee7a59c6068f82afc32c2
Andres Buccella <andresbuccella@gmail.com>
2024-10-07 14:00:20 -0300

    RepoCarrera corregido

--- tp_03_ejercicio_integrador/src/main/java/com/example/tp_03_ejercicio_integrador/repositorios/RepoCarrera.java
+++ tp_03_ejercicio_integrador/src/main/java/com/example/tp_03_ejercicio_integrador/repositorios/RepoCarrera.java
@@ -2,27 +2,28 @@ package com.example.tp_03_ejercicio_integrador.repositorios;
 
 import com.example.tp_03_ejercicio_integrador.model.Carrera;
 import com.example.tp_03_ejercicio_integrador.model.Estudiante;
+import dtos.ReporteCarreraDTO;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository("RepoCarrera")
-public interface RepoCarrera extends RepoBase<Estudiante, Long> {
+public interface RepoCarrera extends RepoBase<Carrera, Long> {
 
     // 2h) Generar un reporte de las carreras, que para cada carrera incluya información de los
     // inscriptos y egresados por año. Se deben ordenar las carreras alfabéticamente, y
     // presentar los años de manera cronológica.
     @Query("SELECT new dtos.ReporteCarreraDTO(c.nombre, " +
-            "YEAR(i.anioInscripcion), " +
-            "YEAR(i.anioEgreso), " +
-            "(SELECT COUNT(i1) FROM Inscripcion i1 WHERE i1.anioEgreso IS NULL AND i1.carrera = c), " + // Inscripciones sin egreso
-            "(SELECT COUNT(i2) FROM Inscripcion i2 WHERE i2.anioEgreso IS NOT NULL AND i2.carrera = c), " + // Inscripciones con egreso
-            "e.id) " +
-            "FROM EstudianteCarrera i " +
-            "JOIN i.carrera c " +
-            "JOIN i.estudiante e " +
-            "ORDER BY c.nombre ASC, YEAR(i.anioInscripcion) ASC, YEAR(i.anioEgreso) ASC")
-    List<Carrera> getReporteCarreras();
+            "YEAR(ec.anioInscripcion), " +
+            "YEAR(ec.anioEgreso), " +
+            "(SELECT COUNT(ec1) FROM EstudianteCarrera ec1 WHERE ec1.anioEgreso IS NULL AND ec1.carrera = c), " + // Inscripciones sin egreso
+            "(SELECT COUNT(ec2) FROM EstudianteCarrera ec2 WHERE ec2.anioEgreso IS NOT NULL AND ec2.carrera = c), " + // Inscripciones con egreso
+            "e.dni) " +
+            "FROM EstudianteCarrera ec " +
+            "JOIN ec.carrera c " +
+            "JOIN ec.estudiante e " +
+            "ORDER BY c.nombre ASC, YEAR(ec.anioInscripcion) ASC, YEAR(ec.anioEgreso) ASC")
+    List<ReporteCarreraDTO> getReporteCarreras();
 
 }
