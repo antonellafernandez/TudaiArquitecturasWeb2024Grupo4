9110a8aca39619d8238f76dd3a380ea1c15ca8d9
Andres Buccella <andresbuccella@gmail.com>
2024-10-27 21:04:08 -0300

    Primer commit

---
+++ TPE/microservicio_monopatin/src/test/java/com/example/microservicio_monopatin/MicroservicioMonopatinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_monopatin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioMonopatinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ TPE/microservicio_viaje/src/test/java/com/example/microservicio_viaje/MicroservicioViajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_viaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioViajeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
