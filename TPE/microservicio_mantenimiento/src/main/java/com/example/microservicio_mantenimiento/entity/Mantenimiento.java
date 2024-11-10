4b1e23203e721ee173d13a40e670c7f9379f3da0
Daniela Fernández <liquiddookie.17@gmail.com>
2024-11-10 16:11:45 -0300

    Updated.

--- TPE/microservicio_mantenimiento/src/main/java/com/example/microservicio_mantenimiento/entity/Mantenimiento.java
+++ TPE/microservicio_mantenimiento/src/main/java/com/example/microservicio_mantenimiento/entity/Mantenimiento.java
@@ -1,12 +1,12 @@
 package com.example.microservicio_mantenimiento.entity;
 
-import com.example.microservicio_monopatin.entity.Monopatin;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
@@ -16,11 +16,24 @@ public class Mantenimiento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    //Revisar
-    @ManyToOne
-    @JoinColumn(name = "monopatin_id", nullable = false)
-    private Long idMonopatin;
+
+    @ElementCollection
+    @CollectionTable(name = "mantenimientoMonopatines", joinColumns = @JoinColumn(name = "mantenimientoId"))
+    @Column(name = "idMonopatin")
+    private List<Long> idMonopatin;
 
     private LocalDateTime fechaInicio;
     private LocalDateTime fechaFin;
+
+    // Método para agregar un monopatín por su ID
+    public void agregarMonopatin(Long monopatinId) {
+        if (!idMonopatines.contains(monopatinId)) {
+            idMonopatines.add(monopatinId);
+        }
+    }
+
+    // Método para quitar un monopatín por su ID
+    public void quitarMonopatin(Long monopatinId) {
+        idMonopatines.remove(monopatinId);
+    }
 }
