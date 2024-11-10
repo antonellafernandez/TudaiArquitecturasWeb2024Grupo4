5e31968a7b68f06ee5ce32c58313abb434ec3bec
Andres Buccella <andresbuccella@gmail.com>
2024-11-10 01:15:11 -0300

    Administrador habilita y deshabilita cuentas

--- TPE/microservicio_administrador/src/main/java/com/example/microservicio_administrador/feignClient/CuentaFeignClient.java
+++ TPE/microservicio_administrador/src/main/java/com/example/microservicio_administrador/feignClient/CuentaFeignClient.java
@@ -2,22 +2,21 @@ package com.example.microservicio_administrador.feignClient;
 
 import com.example.microservicio_administrador.model.Cuenta;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @FeignClient(name = "microservicio_cuenta", url="")
 public interface CuentaFeignClient {
 
     @GetMapping("api/cuentas/")
-    List<Cuenta> getAll();
+    ResponseEntity<?> getAll();
 
     @PostMapping("api/cuentas")
-    Cuenta save(@RequestBody Cuenta cuenta);
+    ResponseEntity<?> save(@RequestBody Cuenta cuenta);
 
     @DeleteMapping("api/cuentas/{id}")
-    void anularCuenta(@PathVariable("id") Long id);
+    ResponseEntity<?> deshabilitarCuenta(@PathVariable("id") Long id);
 
     @PutMapping("api/cuentas/{id}")
-    void restablecerCuenta(@PathVariable("id") Long id);
+    ResponseEntity<?> habilitarCuenta(@PathVariable("id") Long id);
 }
