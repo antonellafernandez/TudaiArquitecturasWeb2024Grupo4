51f7da34a8ef36cbb46d90e880d7de99daf1fae8
Daniela Fernández <liquiddookie.17@gmail.com>
2024-11-11 16:41:41 -0300

    Updated.

--- TPE/microservicio_administrador/src/main/java/com/example/microservicio_administrador/controller/MonopatinController.java
+++ TPE/microservicio_administrador/src/main/java/com/example/microservicio_administrador/controller/MonopatinController.java
@@ -6,17 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/administrador/monopatines")
 public class MonopatinController {
 
     @Autowired
-    private MonopatinFeignClient paradaFeignClient;
+    private MonopatinFeignClient monopatinFeignClient;
 
+    /*
+    3e. Como administrador quiero consultar la cantidad de monopatines actualmente en operación,
+    versus la cantidad de monopatines actualmente en mantenimiento.
+    */
+    @GetMapping("/enOperacion")
+    public ResponseEntity<String> consultarEnOperacion() {
+        List<Monopatin> monopatines = monopatinFeignClient.getAll();
+
+        if (monopatines == null || monopatines.isEmpty()) {
+            return ResponseEntity.ok("No hay monopatines disponibles.");
+        }
+
+        long enOperacion = monopatines.stream()
+                .filter(monopatin -> Boolean.TRUE.equals(monopatin.getDisponible()) ||
+                        (Boolean.FALSE.equals(monopatin.getDisponible()) && monopatin.getViajeActivo() != null))
+                .count();
+
+        long enMantenimiento = monopatines.size() - enOperacion;
+
+        String resultado = String.format("Monopatines en operación: %d, Monopatines en mantenimiento: %d", enOperacion, enMantenimiento);
+        return ResponseEntity.ok(resultado);
+    }
 
     @PostMapping("/")
     public ResponseEntity<Monopatin> addMonopatin(@RequestBody Monopatin newMonopatin) {
-        Monopatin monopatin = paradaFeignClient.save(newMonopatin);
+        Monopatin monopatin = monopatinFeignClient.save(newMonopatin);
         if (monopatin == null)
             return ResponseEntity.notFound().build();
 
@@ -25,7 +49,7 @@ public class MonopatinController {
 
     @PutMapping("/habilitar/{id}")
     public ResponseEntity<Monopatin> habilitarMonopatin(@PathVariable Long id){
-        Monopatin monopatin = paradaFeignClient.habilitarMonopatin(id);
+        Monopatin monopatin = monopatinFeignClient.habilitarMonopatin(id);
         if (monopatin == null)
             return ResponseEntity.notFound().build();
 
@@ -34,7 +58,7 @@ public class MonopatinController {
 
     @PutMapping("/deshabilitar/{id}")
     public ResponseEntity<Monopatin>deshabilitarMonopatin(@PathVariable Long id){
-        Monopatin monopatin = paradaFeignClient.deshabilitarMonopatin(id);
+        Monopatin monopatin = monopatinFeignClient.deshabilitarMonopatin(id);
         if (monopatin == null)
             return ResponseEntity.notFound().build();
 
@@ -43,7 +67,7 @@ public class MonopatinController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Monopatin>updateMonopatin(@PathVariable Long id, @RequestBody Monopatin newMonopatin) {
-        Monopatin monopatin = paradaFeignClient.updateMonopatin(id, newMonopatin);
+        Monopatin monopatin = monopatinFeignClient.updateMonopatin(id, newMonopatin);
         if (monopatin == null)
             return ResponseEntity.notFound().build();
 
--- TPE/microservicio_administrador/src/main/java/com/example/microservicio_administrador/model/Monopatin.java
+++ TPE/microservicio_administrador/src/main/java/com/example/microservicio_administrador/model/Monopatin.java
@@ -1,6 +1,5 @@
 package com.example.microservicio_administrador.model;
 
-import jakarta.persistence.Entity;
 import lombok.*;
 
 @Getter
