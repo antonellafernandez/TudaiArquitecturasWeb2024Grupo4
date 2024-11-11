4384fd65fab66bd51e91e3a0a8aa48ea36e37ded
Andres Buccella <andresbuccella@gmail.com>
2024-11-11 20:58:42 -0300

    application.properties actualizado

--- TPE/microservicio_monopatin/src/main/java/com/example/microservicio_monopatin/feignClient/ParadaFeignClient.java
+++ TPE/microservicio_monopatin/src/main/java/com/example/microservicio_monopatin/feignClient/ParadaFeignClient.java
@@ -1,6 +1,6 @@
 package com.example.microservicio_monopatin.feignClient;
 
-import com.example.microservicio_parada.entity.Parada;
+import com.example.microservicio_monopatin.models.Parada;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
